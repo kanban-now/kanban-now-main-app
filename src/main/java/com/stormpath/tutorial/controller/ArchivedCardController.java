16373257ea446795eebd1575101495f421028223
Mike DuVall <mike.a.duvall@gmail.com>
2016-01-21 12:40:18 -0600

    Updated to pass userid to archive card service.  Unhard code url.  Add integration test support in gradle

--- src/main/java/com/stormpath/tutorial/controller/ArchivedCardController.java
+++ src/main/java/com/stormpath/tutorial/controller/ArchivedCardController.java
@@ -1,13 +1,13 @@
 /*
  * Copyright 2015, Michael Alan DuVall
- * 
+ *
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,6 +19,7 @@
 package com.stormpath.tutorial.controller;
 
 import com.stormpath.sdk.account.Account;
+import com.stormpath.sdk.impl.account.DefaultAccount;
 import com.stormpath.sdk.servlet.account.AccountResolver;
 import com.stormpath.tutorial.exception.ForbiddenException;
 import org.springframework.http.HttpEntity;
@@ -36,18 +37,23 @@ import java.util.List;
 @RestController
 public class ArchivedCardController {
 
-    private static String accessKeyId  = System.getenv("stormpath_apiKey_id");
-    private static String secretKey = System.getenv("stormpath_apiKey_secret");
-    private static String userAndPassword = accessKeyId + ":" + secretKey;
+    private static String kanbanNowServicesAccessKeyId  = System.getenv("kanban_now_services_apiKey_id");
+    private static String kanbanNowServicesSecretKey = System.getenv("kanban_now_services_apiKey_secret");
+
+    private static String userAndPassword = kanbanNowServicesAccessKeyId + ":" + kanbanNowServicesSecretKey;
     private static String userCredentials = userAndPassword;
     private static String basicAuth = "Basic " + DatatypeConverter.printBase64Binary(userCredentials.getBytes());
+
+    private static String archiveCardServiceBaseUrl = System.getenv("archive_card_service_base_url");
 
     @RequestMapping("/api/archived-cards")
     public List<ArchivedCard> greeting(HttpServletRequest req) {
         Account account = AccountResolver.INSTANCE.getAccount(req);
         if (account == null) { throw new ForbiddenException(); }
 
-        String url = "https://archive-card-service-2-development.cfapps.io:443/archivedCards";
+        String userStormpathId = getStormpathIdForAccount(account);
+        String url =archiveCardServiceBaseUrl + "/" + userStormpathId;
+
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authorization", basicAuth);
@@ -57,4 +63,14 @@ public class ArchivedCardController {
         List<ArchivedCard> archivedCardList = Arrays.asList(archivedCardArray);
         return archivedCardList;
     }
+
+
+    private String getStormpathIdForAccount(Account account) {
+        DefaultAccount defaultAccount = (DefaultAccount) account;
+        String href = defaultAccount.getHref();
+        int lastSlash = href.lastIndexOf("/");
+        String id = href.substring(lastSlash + 1, href.length());
+        return id;
+    }
+
 }
