e254df2176c335717392cfaa9147d09614a2fd06
Mike DuVall <mike.a.duvall@gmail.com>
2016-03-23 16:35:40 -0500

    Switched out to paged archived cards list.  Deleted old one

--- src/main/resources/static/js/archived-cards-list.js
+++ src/main/resources/static/js/archived-cards-list.js
@@ -5,28 +5,6 @@ function sleep(delay) {
 }
 
 
-angular.module('kanbannow-archive-cards', []).controller('ArchivedCardListController', function($scope, $http) {
-
-    //$scope.cardList = [{text: "Card1", date: "Date1"},  {text: "Card2", date: "Date2"} /*,*/];
-    var getArchivedCardsUri = 'api/archived-cards';
-
-    $scope.errorGettingArchivedCardList = false;
-    $scope.errorGettingArchivedCardListErrorMessage = "";
-
-    $http({
-        method: 'GET',
-        url: getArchivedCardsUri
-    }).then(function successCallback(response) {
-        $scope.cardList = response.data;
-        sleep(5000)
-    }, function errorCallback(response) {
-        $scope.errorGettingArchivedCardList = true;
-        $scope.errorGettingArchivedCardListErrorMessage = response.data;
-    });
-
-});
-
-
 angular.module('ngTableTutorial', ['ngTable']).controller('tableController', function($scope, $filter, $http, ngTableParams) {
 
     var getArchivedCardsUri = 'api/new-archived-cards';
